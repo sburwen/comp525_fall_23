735ef518ef46e7aa1501f2b318068a4947f0d5c4
Sean <seb1182@unh.edu>
2023-10-06 19:50:12 -0400

    edited to initialize lists locally

--- homework/hw1/tests.py
+++ homework/hw1/tests.py
@@ -5,16 +5,12 @@ Date: 10/6/2023
 """
 import my_list
 
-test_list1 = [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108]
-test_list2 = [4, 6, 15, 15, 22, 34, 49, 49, 65, 71]
-test_list3 = [0, 1, 2, 3, 4, 5]
-
-
 def count_test():
     """
     Test inputs for count() function
     :return: integer, representing how many times `num` is in `num_list`
     """
+    test_list1 = [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108]
     # Expected output 1:
     print(F"Count Test 1: {my_list.count(test_list1, 9)}")
 
@@ -30,10 +26,14 @@ def extend_test():
     Test inputs for extend() function
     :return: list of integers combined
     """
+    test_list1 = [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108]
+    test_list2 = [4, 6, 15, 15, 22, 34, 49, 49, 65, 71]
+    test_list3 = [0, 1, 2, 3, 4, 5]
     # Expected output [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108, 4, 6, 15, 15, 22, 34, 49, 49, 65, 71]:
     print(F"Extend Test 1: {my_list.extend(test_list1, test_list2)}")
 
-    # Expected output [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108, 0, 1, 2, 3, 4, 5]:
+    # Expected output [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108, 4, 6, 15, 15, 22, 34, 49, 49, 65, 71, 0,
+    # 1, 2, 3, 4, 5]:
     print(F"Extend Test 2: {my_list.extend(test_list1, test_list3)}")
 
     # Expected output [4, 6, 15, 15, 22, 34, 49, 49, 65, 71, 0, 1, 2, 3, 4, 5]:
@@ -45,6 +45,7 @@ def remove_test():
     Test inputs for remove() function
     :return: list of integers, with num removed
     """
+    test_list2 = [4, 6, 15, 15, 22, 34, 49, 49, 65, 71]
     # Expected output [4, 6, 15, 15, 22, 34, 65, 71]:
     print(F"Remove Test 1: {my_list.remove(test_list2, 49)}")
 
@@ -62,6 +63,7 @@ def index_test():
     `num`
     :return: None, if `num` not found
     """
+    test_list1 = [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108]
     # Expected output 8:
     print(F"Index Test 1: {my_list.index(test_list1, 76)}")
 
