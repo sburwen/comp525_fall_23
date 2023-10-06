31c7a050d7bd733d2e13d6397658f384f97b1fa9
Sean <seb1182@unh.edu>
2023-10-06 19:32:10 -0400

    cleaning it up

--- homework/hw1/tests.py
+++ homework/hw1/tests.py
@@ -16,13 +16,13 @@ def count_test():
     :return: integer, representing how many times `num` is in `num_list`
     """
     # Expected output 1:
-    print(my_list.count(test_list1, 9))
+    print(F"Count Test 1: {my_list.count(test_list1, 9)}")
 
     # Expected output 2:
-    print(my_list.count(test_list1, 24))
+    print(F"Count Test 2: {my_list.count(test_list1, 24)}")
 
     # Expected output 0:
-    print(my_list.count(test_list1, 1))
+    print(F"Count Test 3: {my_list.count(test_list1, 1)}")
 
 
 def extend_test():
@@ -31,13 +31,13 @@ def extend_test():
     :return: list of integers combined
     """
     # Expected output [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108, 4, 6, 15, 15, 22, 34, 49, 49, 65, 71]:
-    print(my_list.extend(test_list1, test_list2))
+    print(F"Extend Test 1: {my_list.extend(test_list1, test_list2)}")
 
     # Expected output [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108, 0, 1, 2, 3, 4, 5]:
-    print(my_list.extend(test_list1, test_list3))
+    print(F"Extend Test 2: {my_list.extend(test_list1, test_list3)}")
 
     # Expected output [4, 6, 15, 15, 22, 34, 49, 49, 65, 71, 0, 1, 2, 3, 4, 5]:
-    print(my_list.extend(test_list2, test_list3))
+    print(F"Extend Test 3: {my_list.extend(test_list2, test_list3)}")
 
 
 def remove_test():
@@ -46,13 +46,13 @@ def remove_test():
     :return: list of integers, with num removed
     """
     # Expected output [4, 6, 15, 15, 22, 34, 65, 71]:
-    print(my_list.remove(test_list2, 49))
+    print(F"Remove Test 1: {my_list.remove(test_list2, 49)}")
 
     # Expected output [6, 15, 15, 22, 34, 49, 49, 65, 71]:
-    print(my_list.remove(test_list2, 4))
+    print(F"Remove Test 2: {my_list.remove(test_list2, 4)}")
 
     # Expected output [4, 6, 15, 15, 22, 34, 49, 49, 65, 71]:
-    print(my_list.remove(test_list2, 2))
+    print(F"Remove Test 3: {my_list.remove(test_list2, 2)}")
 
 
 def index_test():
@@ -63,13 +63,13 @@ def index_test():
     :return: None, if `num` not found
     """
     # Expected output 8:
-    print(my_list.index(test_list1, 76))
+    print(F"Index Test 1: {my_list.index(test_list1, 76)}")
 
     # Expected output 3:
-    print(my_list.index(test_list1, 12))
+    print(F"Index Test 2: {my_list.index(test_list1, 12)}")
 
     # Expected output None:
-    print(my_list.index(test_list1, 109))
+    print(F"Index Test 3: {my_list.index(test_list1, 109)}")
 
 
 count_test()
