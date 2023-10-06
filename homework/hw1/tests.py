2260f15e28a513162ddd9e2d5c6769c5207b8ba0
Sean <seb1182@unh.edu>
2023-10-06 19:18:56 -0400

    extend() function test cases

--- homework/hw1/tests.py
+++ homework/hw1/tests.py
@@ -7,6 +7,7 @@ import my_list
 
 test_list1 = [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108]
 test_list2 = [4, 6, 15, 15, 22, 34, 49, 49, 65, 71]
+test_list3 = [0, 1, 2, 3, 4, 5]
 
 
 def count_test():
@@ -22,3 +23,18 @@ def count_test():
 
     # Expected output 0:
     print(my_list.count(test_list1, 1))
+
+
+def extend_test():
+    """
+    Test inputs for extend() function
+    :return: list of integers combined
+    """
+    # Expected output [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108, 4, 6, 15, 15, 22, 34, 49, 49, 65, 71]:
+    print(my_list.extend(test_list1, test_list2))
+
+    # Expected output [3, 8, 9, 12, 24, 24, 31, 52, 76, 76, 99, 108, 0, 1, 2, 3, 4, 5]:
+    print(my_list.count(test_list1, test_list3))
+
+    # Expected output [4, 6, 15, 15, 22, 34, 49, 49, 65, 71, 0, 1, 2, 3, 4, 5]:
+    print(my_list.count(test_list2, test_list3))
