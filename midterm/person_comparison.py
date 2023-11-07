cc3ffc3c338a0f10630b2ad5eff72ce244086369
jax472 <jax472@wildcats.unh.edu>
2023-11-07 14:30:23 +0000

    added midterm

---
+++ midterm/person_comparison.py
@@ -0,0 +1,12 @@
+"""
+Create a Python class Person with attributes for a person's name and age. 
+Implement a method to compare the ages of two people, returning a message that 
+indicates who is older. For example, if person1 is 30 years old and person2 is 
+25 years old, calling person1.compare_age(person2) should return 
+"person1 is older."
+
+- Use appropriate function docstrings. 
+- Include the time complexity of your function in your docstring.
+- No need for design documents, unit tests, etc. Just the function and a call
+to the function.
+"""
